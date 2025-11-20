c86e244192503fdc5661acfff25440d930d140b0
Tim Vanloocke <Tim@skilate.local>
2025-11-20 15:43:12 +0100

    tlectrics name change

---
+++ js/data.js
@@ -0,0 +1,6 @@
+export var diensten = [
+    {
+        "icon": "fa-plug",
+        "title": "Automatisatie deuren + poorten",
+        "text": "Credit goes to Pexels website for all images used in this template. Cras condimentum mi et sapien dignissim luctus."
+    }, {}]
